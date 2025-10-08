fbddd7f5489538b2ed8157967dd1ab91f6173ada
Robert Engel <robert.engel@apollidon.com>
2025-10-08 06:24:19 -0400

    Add and config prettier with eslint for code formatting.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,7 +16,14 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig, // Disables ESLint rules that conflict with Prettier
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Run Prettier as an ESLint rule
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
